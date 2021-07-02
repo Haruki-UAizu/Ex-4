0e8fb2db2af24cecffbd53236674ccc018c5a607
Haruki-UAizu <s1280083@u-aizu.ac.jp>
2021-07-02 11:31:45 +0900

    Initial version

---
+++ ex04.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class ex04 {
+    public static void main(String[] args){
+	int i = 0;
+	int j = 0;
+	Random rand = new Random();
+	//Scanner scanner = new Scanner(System.in);
+
+	System.out.println("Rolling the dice...");
+	i = rand.nextInt(6) + 1;
+	j = rand.nextInt(6) + 1;
+	
+	System.out.println("Die 1: " + i);
+	System.out.println("Die 2: " + j);
+	System.out.println("Total value: " + (i + j));
+    }
+}
