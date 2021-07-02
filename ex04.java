0b0138f4feed533d93310e914f0263a5f8f0d770
Haruki-UAizu <s1280083@u-aizu.ac.jp>
2021-07-02 11:47:52 +0900

    Greeting added

--- ex04.java
+++ ex04.java
@@ -5,9 +5,16 @@ class ex04 {
     public static void main(String[] args){
 	int i = 0;
 	int j = 0;
+	String name;
 	Random rand = new Random();
-	//Scanner scanner = new Scanner(System.in);
+	Scanner scanner = new Scanner(System.in);
 
+	//Asking Username
+	System.out.println("What is your name?");
+	name = scanner.next();
+	System.out.println("Hello, " + name + "!");
+
+	//Rolling dice
 	System.out.println("Rolling the dice...");
 	i = rand.nextInt(6) + 1;
 	j = rand.nextInt(6) + 1;
